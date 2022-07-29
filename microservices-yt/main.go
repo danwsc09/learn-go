50a425abac24dcea87b26136d87c27f725e814d1
Wonsang <temporaryaion1@hotmail.com>
2022-07-29 23:00:04 +0900

    microservices: basic web server

--- microservices-yt/main.go
+++ microservices-yt/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("HEY THERE!"))
 		log.Println("Hello World!")
+		d, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			http.Error(w, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "Hello %s\n", d)
+	})
+
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("HEY THERE!"))
+		log.Println("Goodbye World!")
 	})
 
 	// bind to every IP (:9090); constructs an HTTP server and registers a default handler
