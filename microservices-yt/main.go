a213a110ec17e98823fcd30e942d4142c27ce698
Wonsang <temporaryaion1@hotmail.com>
2022-07-30 09:30:25 +0900

    shut down server gracefully with signals

--- microservices-yt/main.go
+++ microservices-yt/main.go
@@ -1,30 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/danwsc09/learn-go/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		log.Println("Hello World!")
-		d, err := ioutil.ReadAll(req.Body)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	// various settings like Idle/Read/Write Timeout, Logger
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	// so it doesn't block
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(w, "Oops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
+	}()
 
-		fmt.Fprintf(w, "Hello %s\n", d)
-	})
+	// notify the channel on Interrupt/Kill signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-	http.HandleFunc("/goodbye", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("HEY THERE!"))
-		log.Println("Goodbye World!")
-	})
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown.", sig)
 
-	// bind to every IP (:9090); constructs an HTTP server and registers a default handler
-	// default serve mux
-	http.ListenAndServe(":9090", nil)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
