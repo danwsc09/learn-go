e550854dee18e221a6cd8dd749abcdc9b43efcb2
Wonsang <temporaryaion1@hotmail.com>
2022-07-30 15:44:51 +0900

    handle POST request with unmarshaling

--- microservices-yt/handlers/products.go
+++ microservices-yt/handlers/products.go
@@ -21,6 +21,10 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	// handle an update
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
 
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
@@ -33,6 +37,20 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	product := &data.Product{}
+
+	err := product.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+		return
+	}
+
+	p.l.Printf("Prod: %#v\n", product)
+	data.AddProduct(product)
+	rw.WriteHeader(http.StatusCreated)
 }
 
 // NewEncoder
